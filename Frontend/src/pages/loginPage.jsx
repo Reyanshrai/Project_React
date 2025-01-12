29ce9dff4bf5550e0baef28481c9479366a59cf9
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-01-13 01:00:11 +0530

    add header to login page

--- Frontend/src/pages/loginPage.jsx
+++ Frontend/src/pages/loginPage.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+
 
 const loginPage = () => {
   return (
     <div>
-      
+      <h1 className="text-center text-2xl text-red-900">I am login page</h1>
     </div>
   )
 }
