2e890df8b7e0a0905adaae48e5499328982d15b2
VSah7079 <vk3815155@gmail.com>
2025-01-13 23:33:33 +0530

    Add route in navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { Link , useNavigate} from "react-router-dom";
-
-=======
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
->>>>>>> 1893ec31cadd241443cf85876891c82abfe97e8a
+import { useNavigate, Link } from "react-router-dom";
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
@@ -15,7 +10,7 @@ const Navbar = () => {
   };
 
   const handleGetStarted = () => {
-    navigate("/register")
+    navigate("/register");
   };
 
   return (
@@ -49,7 +44,7 @@ const Navbar = () => {
           {/* Get Started Button and Mobile Menu Toggle */}
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
-            onClick={handleGetStarted}
+              onClick={handleGetStarted}
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
@@ -114,5 +109,4 @@ const Navbar = () => {
     </div>
   );
 };
-
 export default Navbar;
