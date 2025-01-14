afa4abe360691167275ad37d3f3466f66f4555f1
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-01-15 00:19:36 +0530

    refactor: update App component to use new routing system

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,7 @@
-import {Routes,Route} from 'react-router-dom'
-import { Home, About, Services, Timetable, Trainer, Pricing, Contact, Login, Register, ForgotPassword } from './pages';
+import AppRoutes from "./routes/AppRoutes";
+
 const App = () => {
-  return (
-    <>
-      <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path ='/about' element={<About/>}  />
-        <Route path='/services' element={<Services />} />
-        <Route path='/timetable' element={<Timetable />} />
-        <Route path='/trainer' element={<Trainer />} />
-        <Route path='/pricing' element={<Pricing />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/forgotpassword' element={<ForgotPassword />} />
-      </Routes>
-      </div>
-    </>
-  )
-}
+  return <AppRoutes />;
+};
 
 export default App;
