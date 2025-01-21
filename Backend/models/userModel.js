a24c64e03862937d2743234e577b879cd65728ca
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-01-21 13:04:05 +0530

    Comment out password matching and token generation methods in userModel

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -39,15 +39,15 @@ const userSchema = new mongoose.Schema(
     }
 );
 
-userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-};
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//     return await bcrypt.compare(enteredPassword, this.password);
+// };
 
-userSchema.methods.generateToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-        expiresIn: '1d',
-    });
-};
+// userSchema.methods.generateToken = function () {
+//     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+//         expiresIn: '1d',
+//     });
+// };
 
 const User = mongoose.model('User', userSchema);
 
