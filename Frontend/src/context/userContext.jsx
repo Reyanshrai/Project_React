14eec3835f486eec6aef91294e774866d0dec0e3
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-01-22 17:23:51 +0530

    Refactor UserContext to improve user data validation and formatting

--- Frontend/src/context/userContext.jsx
+++ Frontend/src/context/userContext.jsx
@@ -1,32 +1,34 @@
-import { createContext,useState } from "react";
+import { createContext, useState } from "react";
 import PropTypes from "prop-types";
 
 export const UserContext = createContext();
 
-export const UserProvider = ({children}) => {
-
-    const [user,setUser] = useState(null);
+export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
 
     const login = (userData) => {
+        if (!userData || !userData.token || !userData.firstname) {
+            console.error("Invalid user data");
+            return;
+        }
         setUser(userData);
-        localStorage.setItem("token",userData.token);
-        localStorage.setItem("firstName",JSON.stringify(userData));
-    }
+        localStorage.setItem("token", userData.token);
+        localStorage.setItem("firstname", userData.firstname);
+    };
 
     const logout = () => {
-        setUser(null)
+        setUser(null);
         localStorage.removeItem("token");
-        localStorage.removeItem("firstName");
-    }
-
+        localStorage.removeItem("firstname");
+    };
 
     return (
-        <UserContext.Provider value={{user,login,logout}}>
+        <UserContext.Provider value={{ user, login, logout }}>
             {children}
         </UserContext.Provider>
-    )
-}
+    );
+};
 
 UserProvider.propTypes = {
-    children: PropTypes.node.isRequired
-}
+    children: PropTypes.node.isRequired,
+};
