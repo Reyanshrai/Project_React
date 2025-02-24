bd527ab02c9b49dd8b56f162728b95623f492a86
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-02-24 10:50:52 +0530

    Add index file for Admin page to export components

---
+++ Frontend/src/pages/Admin/index.js
@@ -0,0 +1,6 @@
+export {default as AdminDashboard} from './Dashboard'
+export {default as AdminLogin} from './Adminlogin'
+export {default as AdminMembers} from './Member'
+export {default as AdminPayments} from './Payment'
+export {default as AdminTrainers} from './Trainer'
+
