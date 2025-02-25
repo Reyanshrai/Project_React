bf2c6aed6b33263dc130e1da87a64e6c53ff68d5
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-02-25 13:41:55 +0530

    Add validation for admin registration fields including firstname, lastname, email, and password

--- Backend/validators/adminValidation.js
+++ Backend/validators/adminValidation.js
@@ -1,6 +1,15 @@
 import {body} from 'express-validator'
 
+
+export const validateAdminRegister = [
+    
+    body("firstname").isLength({min:3}).withMessage("Firstname is required"),
+    body("lastname").isLength({min:3}).withMessage("Lastname is required"),
+    body("email").isEmail().withMessage("Email is required"),
+    body("password").isLength({min:4}).withMessage("Password is required"),
+]
+
 export const validateAdminLogin = [
     body("email").isEmail().withMessage("Email is required"),
-    body("password").isLength({min:4}).withMessage("Password is required")
+    body("password").isLength({min:4}).withMessage("Password is required"),
 ]
