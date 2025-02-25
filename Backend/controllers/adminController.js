640710681c80318c30e7638b453508e3d1c14641
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-02-25 13:41:38 +0530

    Implement admin registration functionality with validation, password hashing, and JWT token generation

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -10,8 +10,59 @@ const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 };
 
+// Admin Create Controller
+
+export const adminRegister = asyncHandler(async (req,res) => {
+    // Validate request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(StatusCodes.BAD_REQUEST);
+        throw new Error(errors.array()[0].msg);
+    }
+
+    const { firstname,lastname,email, password } = req.body;
+
+    // Check if admin exists
+    const adminExists = await Admin.findOne({email});
+    if(adminExists){
+        res.status(StatusCodes.CONFLICT);
+        throw new Error('Admin already exists');
+    }
+    
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    // Create admin
+    const admin = await Admin.create({
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword
+    });
+
+    // Generate JWT Token
+
+    const token = generateToken(admin._id);
+
+    // Set token in HTTP-Only Cookie
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development', // Secure cookies in production
+        sameSite: 'none',
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    // Send response
+
+    res.status(StatusCodes.CREATED).json({
+        _id: admin._id,
+        email: admin.email,
+        token,
+    });
+})
+
 // Admin Login Controller
-export const Adminlogin = asyncHandler(async (req, res) => {
+export const adminlogin = asyncHandler(async (req, res) => {
     // Validate request
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -23,6 +74,7 @@ export const Adminlogin = asyncHandler(async (req, res) => {
 
     // Check if admin exists
     const admin = await Admin.findOne({ email });
+
     if (!admin) {
         res.status(StatusCodes.UNAUTHORIZED);
         throw new Error('Invalid email or password');
