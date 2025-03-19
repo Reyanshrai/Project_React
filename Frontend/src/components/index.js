6970476be992a78606ffc5f5a8acb22eb09cf799
reyanshrai <reyanshrai05@gmail.com>
2025-03-19 15:52:21 +0530

    Refactor Admin index exports for consistency and correct component naming

--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -1,4 +1,3 @@
 export {default as Navbar} from './Navbar'
 export {default as Footer} from './Footer'
-// export {default as Carousoul} from './Carosoul'
 export {default as Carosoul} from './Carosoul'
--- Frontend/src/pages/Admin/index.js
+++ Frontend/src/pages/Admin/index.js
@@ -1,8 +1,6 @@
-export {default as AdminDashboard} from './Dashboard'
-export {default as AdminLogin} from './Adminlogin'
-export {default as AdminMembers} from './Member'
-export {default as AdminPayments} from './Payment'
-export {default as AdminTrainers} from './Trainer'
+export {default as AdminDashboard} from './AdminDashboard'
+export {default as AdminLogin} from './AdminLogin'
+export {default as Payments} from './Payment'
 export {default as GymMembers} from './GymMembers'
 export {default as TrainersManagement} from './TrainersManagement'
 
