d19e945cb5953cb450e6be18d614554b58e20aa5
reyanshrai <reyanshrai05@gmail.com>
2025-03-19 11:16:57 +0530

    Add logout route to adminRoutes for handling admin logout functionality

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express'
 import {validateAdminRegister,validateAdminLogin} from '../validators/adminValidation.js'
-import {adminRegister,adminlogin} from '../controllers/adminController.js'
+import {adminRegister,adminlogin,adminLogout} from '../controllers/adminController.js'
 
 
 const router = Router()
 
 router.post("/register",validateAdminRegister,adminRegister)
 router.post("/login",validateAdminLogin,adminlogin)
+router.get("/logout",adminLogout)
 
 export default router
